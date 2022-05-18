1ad9cf498e5ac252caf5737c47a99f3fec1e1652
Lene Svit <lene261r@edu.ucl.dk>
2022-05-18 11:57:38 +0200

    add test

---
+++ UnitTest/MoqTest/TemplateControllerTest.cs
@@ -0,0 +1,72 @@
+using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Moq;
+using Rapport.Api.Controllers;
+using Rapport.BusinessLogig.Interfaces;
+using Rapport.BusinessLogig.Mapping;
+using Rapport.BusinessLogig.Services;
+using Rapport.Entites;
+using Rapport.Shared.Dto_er.Template;
+using System.Threading.Tasks;
+using UnitTest.Extensions;
+using Xunit;
+
+namespace UnitTest.MoqTest
+{
+    public class TemplateControllerTest
+    {
+
+        private readonly TemplateController _controller;
+        private readonly Mock<ILogger<TemplateController>> _logger = new Mock<ILogger<TemplateController>>();
+        private readonly Mock<ITemplateService> _templateService = new Mock<ITemplateService>();
+
+        private readonly Mock<IMapper> _mapperMock = new Mock<IMapper>();
+
+        private int Id;
+        public TemplateControllerTest()
+        {
+            var mockMapper = new MapperConfiguration(cfg => cfg.AddProfile(new MappingProfile()));
+            var mapper = mockMapper.CreateMapper();
+            _controller = new TemplateController(_templateService.Object, _logger.Object, mapper);
+
+        }
+
+
+
+        [Fact]
+        public async Task GetById_WhenTemplateDoesNotExist_ShouldReturnNothing()
+        {
+            //Arrange
+            var templateId = Id;
+
+            _templateService.SetupIgnoreArgs(x => x.GetTemplateById(templateId))
+              .ReturnsAsync(() => null);
+
+            //Act
+            var templateTest = await _controller.GetTemplateByIdAsync(templateId);
+            var result = templateTest.Result as NotFoundResult;
+
+            //Assert
+
+            Assert.Equal(result.StatusCode, StatusCodes.Status404NotFound);
+        }
+
+        //[Fact]
+        //public async Task Create_WhenTemplateIsCreated_ShouldReturnCategory()
+        //{
+        //    var templateName = "Test";
+
+        //    var requestDto = new CreateTemplateDto
+        //    {
+        //        Title = templateName,
+
+        //    };
+
+        //    _templateService.Setup(x => x.CreateTemplate(It.IsAny<Template>()))
+        //         .ReturnsAsync(() => true);
+
+        //}
+    }
+}
---
+++ UnitTest/MoqTest/TemplateServiceTest.cs
@@ -0,0 +1,73 @@
+﻿using AutoMapper;
+using Moq;
+using Rapport.BusinessLogig.Interfaces;
+using Rapport.BusinessLogig.Mapping;
+using Rapport.BusinessLogig.Services;
+using Rapport.Entites;
+using Rapport.Shared.Dto_er.Template;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnitTest.Extensions;
+using Xunit;
+
+namespace UnitTest.MoqTest
+{
+    public class TemplateServiceTest
+    {
+        private readonly TemplateService _sut;
+        private readonly Mock<IGenericRepository<Template>> _repositoryMock = new Mock<IGenericRepository<Template>>();
+
+        private int Id;
+        public TemplateServiceTest()
+        {
+            var mockMapper = new MapperConfiguration(cfg => cfg.AddProfile(new MappingProfile()));
+            var mapper = mockMapper.CreateMapper();
+            _sut = new TemplateService(_repositoryMock.Object, mapper);
+        }
+
+        [Fact]
+        public async Task GetByIdAsync_ShouldReturnTemplate_WhenTemplatExist()
+        {
+            //Arrange
+            var templateId = Id;
+            var templateTitel = "Test";
+
+            var template = new Template
+            {
+                Id = templateId,
+                Title = templateTitel
+            };
+
+            _repositoryMock.SetupIgnoreArgs(x => x.GetAsync(x => x.Id == templateId, null))
+                .ReturnsAsync(template);
+
+            //Act
+            var templateTest = await _sut.GetTemplateById(templateId);
+
+
+            //Assert
+            Assert.Equal(templateTest!.Id, templateId);
+            Assert.Equal(templateTest!.Title, templateTitel);
+
+        }
+
+        [Fact]
+        public async Task Create_WhenTemplateIsCreated_ShouldReturnCategory()
+        {
+            //Arrange
+
+            var templateTitel = "Test";
+
+            var requestDto = new CreateTemplateDto
+            {
+
+                Title = templateTitel
+            };
+           
+
+        }
+    }
+}
