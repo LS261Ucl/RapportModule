8f5c13d4a738b7ff67234908b28d5ede2b2254a4
Lene Svit <lene261r@edu.ucl.dk>
2022-05-18 11:07:29 +0200

    MockTest

---
+++ UnitTest/Extensions/MoqExtensions.cs
@@ -0,0 +1,35 @@
+﻿using Moq;
+using Moq.Language.Flow;
+using System;
+using System.Linq.Expressions;
+
+namespace UnitTest.Extensions
+{
+    public static class MoqExtensions
+    {
+        public static ISetup<T, TResult> SetupIgnoreArgs<T, TResult>(this Mock<T> mock,
+            Expression<Func<T, TResult>> expression)
+            where T : class
+        {
+            expression = new MakeAnyVisitor().VisitAndConvert(
+                expression, "SetupIgnoreArgs");
+
+            return mock.Setup(expression);
+        }
+
+        private class MakeAnyVisitor : ExpressionVisitor
+        {
+            protected override Expression VisitConstant(ConstantExpression node)
+            {
+                if (node.Value != null)
+                    return base.VisitConstant(node);
+
+                var method = typeof(It)
+                    .GetMethod("IsAny")
+                    ?.MakeGenericMethod(node.Type);
+
+                return Expression.Call(method!);
+            }
+        }
+    }
+}
--- UnitTest/TemplateControllerTest.cs
+++ UnitTest/TemplateControllerTest.cs
@@ -1,22 +1,101 @@
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Moq;
 using Rapport.Api.Controllers;
 using Rapport.BusinessLogig.Interfaces;
+using Rapport.BusinessLogig.Mapping;
+using Rapport.BusinessLogig.Services;
 using Rapport.Entites;
+using Rapport.Shared.Dto_er.Template;
+using System.Threading.Tasks;
+using UnitTest.Extensions;
 using Xunit;
 
 namespace UnitTest
 {
     public class TemplateControllerTest
     {
-        private readonly ITemplateService _service;
-        private readonly IGenericRepository<Template> _repository;
-        private readonly ILogger<TemplateController> _logger;
+        private readonly TemplateService _sut;
+        private readonly Mock<IGenericRepository<Template>> _repositoryMock = new Mock<IGenericRepository<Template>>();
 
+        private readonly Mock<IMapper> _mapperMock = new Mock<IMapper>();
+
+        private int Id;
+        public TemplateControllerTest()
+        {
+            var mockMapper = new MapperConfiguration(cfg => cfg.AddProfile(new MappingProfile()));
+            var mapper = mockMapper.CreateMapper();
+            _sut = new TemplateService( _repositoryMock.Object, mapper);
+        }
 
         [Fact]
-        public void Test1()
+        public async Task GetByIdAsync_ShouldReturnTemplate_WhenTemplatExist()
         {
+            //Arrange
+            var templateId = Id;
+            var templateTitel = "Test";
+            
+            var template = new Template
+            {
+                Id = templateId,
+                Title = templateTitel
+            };
+
+            _repositoryMock.SetupIgnoreArgs(x => x.GetAsync(x => x.Id == templateId, null))
+                .ReturnsAsync(template);
+
+            //Act
+            var templateTest = await _sut.GetTemplateById(templateId);
+
+
+            //Assert
+            Assert.Equal(templateTest!.Id, templateId);
+            Assert.Equal(templateTest!.Title, templateTitel);
 
         }
+
+        //[Fact]
+        //public async Task GetById_WhenTemplateDoesNotExist_ShouldReturnNothing()
+        //{
+        //    //Arrange
+        //    var templateId = Id;
+
+        //    _repositoryMock.SetupIgnoreArgs(x => x.GetAsync(x => x.Id == templateId, null))
+        //      .ReturnsAsync(()=> null);
+
+        //    //Act
+        //    var templateTest = await _sut.GetTemplateById(templateId);
+        //    var result = templateTest.Return as NotFoundResult;
+
+        //    //Assert
+
+        //    Assert.Equal(result.StatusCode, StatusCodeResult.)
+        //}
+
+        //[Fact]
+        //public async Task Create_WhenTemplateIsCreated_ShouldReturnCategory()
+        //{
+        //    var templateName = "Test";
+
+        //    var requestDto = new CreateTemplateDto
+        //    {
+        //        Title = templateName,
+             
+        //    };
+
+
+        //    _repositoryMock.Setup(x => x.CreateAsync(It.IsAny<Template>()))
+        //       .ReturnsAsync(() => true);
+
+
+        //    var response = await _sut.CreateTemplate(requestDto);
+
+        //    var result = response.Result as CreatedAtActionResult;
+        //    var category = result?.Value as ProductCategoryDto;
+
+        //    Assert.Equal(categoryName, category?.Name);
+        //    Assert.Equal(StatusCodes.Status201Created, result?.StatusCode);
+        //}
     }
 }
